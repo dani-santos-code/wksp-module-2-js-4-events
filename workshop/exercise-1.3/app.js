e95118f7c9db14fb3ee5bfcdff64faec11d609d1
Dani Santos <santos.adaniele@gmail.com>
2020-02-06 14:47:49 -0500

    exercise 1.3

--- workshop/exercise-1.3/app.js
+++ workshop/exercise-1.3/app.js
@@ -5,3 +5,28 @@
 // a random amount of time (up to 5 seconds) to click the screen.
 
 // It would be a good idea to create a new function that will manage the whole game.
+
+const startGame = () => {
+  const body = document.querySelector("body");
+  const result = document.querySelector("#result");
+  let hasWon = false;
+  const handleClick = () => {
+    hasWon = true;
+    result.innerText = "You Win";
+    body.removeEventListener("click", handleClick);
+  };
+
+  body.addEventListener("click", handleClick);
+
+  const randomTime = Math.floor(Math.random() * 5000);
+  document.querySelector("#timer").innerText = Math.round(randomTime / 100);
+
+  setTimeout(() => {
+    if (!hasWon) {
+      result.innerText = "You Lose";
+      body.removeEventListener("click", handleClick);
+    }
+  }, randomTime);
+};
+
+startGame();
