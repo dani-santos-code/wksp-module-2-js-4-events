0f0f9a55517e81e5b6a2b55bec5205e2faae4d2f
Dani Santos <santos.adaniele@gmail.com>
2020-02-09 20:58:31 -0500

    restart button
    still not working as it should, but better than previous versions

--- workshop/exercise-5-boss-level/script.js
+++ workshop/exercise-5-boss-level/script.js
@@ -1,6 +1,7 @@
 const instructionsDialog = document.getElementById("instructions");
 const gamePlay = document.getElementById("game");
 const menuDiv = document.getElementById("menu");
+const container = document.getElementById("container");
 const prize = document.getElementById("prize");
 const signalDiv = document.getElementById("signal");
 const signal = new Audio("./sounds/signal.mp3");
@@ -8,6 +9,7 @@ const p1Div = document.getElementById("p1-score");
 const p2Div = document.getElementById("p2-score");
 const results = document.querySelector("#results");
 const h1 = document.querySelector("h1");
+let hasStarted = false;
 
 const MAX_SCORE = 3;
 let p1Score = 0;
@@ -15,6 +17,7 @@ let p2Score = 0;
 
 const showInstructions = () => {
   instructionsDialog.style.display = "flex";
+  moveCharacters();
 };
 
 const closeInstructions = () => {
@@ -25,29 +28,47 @@ const checkWinner = () => {
   if (p1Score === MAX_SCORE) {
     results.style.display = "block";
     h1.innerText = "Pi Wins!";
+    document.removeEventListener("keydown", handleKeyPress);
   } else if (p2Score === MAX_SCORE) {
     results.style.display = "block";
+    h1.innerText = "Sonic Wins!";
+    document.removeEventListener("keydown", handleKeyPress);
   }
 };
+
 const handleKeyPress = () => {
   if (event.key === "q" || event.key === "Q") {
-    p1Score += 1;
-    if (p1Score <= MAX_SCORE) {
-      const image = document.createElement("img");
-      p1Div.appendChild(image);
-      image.setAttribute("src", "./imgs/prize.gif");
-      checkWinner();
+    if (hasStarted) {
+      p1Score += 1;
+      if (p1Score <= MAX_SCORE) {
+        const image = document.createElement("img");
+        p1Div.appendChild(image);
+        image.setAttribute("src", "./imgs/prize.gif");
+        checkWinner();
+      }
+    } else {
+      endGameImmediately("Pi");
     }
   }
   if (event.key === "p" || event.key === "P") {
-    p2Score += 1;
-    if (p2Score <= MAX_SCORE) {
-      const image = document.createElement("img");
-      p2Div.appendChild(image);
-      image.setAttribute("src", "./imgs/prize.gif");
-      checkWinner();
+    if (hasStarted) {
+      p2Score += 1;
+      if (p2Score <= MAX_SCORE) {
+        const image = document.createElement("img");
+        p2Div.appendChild(image);
+        image.setAttribute("src", "./imgs/prize.gif");
+        checkWinner();
+      }
+    } else {
+      endGameImmediately("Sonic");
     }
   }
+};
+
+const endGameImmediately = player => {
+  results.style.display = "block";
+  h1.innerText = `${player}, you Lose!`;
+  document.removeEventListener("keydown", handleKeyPress);
 };
 
 const moveCharacters = () => {
@@ -55,12 +76,14 @@ const moveCharacters = () => {
 };
 
 const start = () => {
+  container.removeEventListener("keydown", handleKeyPress);
   gamePlay.style.display = "block";
   prize.style.display = "inline-block";
   menuDiv.style.display = "none";
   menuDiv.style.position = "relative";
   moveCharacters();
   setTimeout(() => {
+    hasStarted = !hasStarted;
     const image = document.createElement("img");
     image.style.width = "100px";
     image.style.height = "100px;";
@@ -77,11 +100,12 @@ const start = () => {
 };
 
 const restart = () => {
+  hasStarted = false;
+  container.removeEventListener("keydown", handleKeyPress);
   gamePlay.style.display = "none";
   prize.style.display = "none";
   results.style.display = "none";
   p1Div.style.display = "none";
   p2Div.style.display = "none";
-  document.querySelector("#signal-to-play").style.display = "none";
   menuDiv.style.display = "block";
 };
